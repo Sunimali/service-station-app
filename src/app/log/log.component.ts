4ae1b1e8f79fe540017dd8ecb8d8f450e23b3e62
Sunimali Rathnayake <sunimalir@localhost.localdomain>
2019-06-20 07:41:43 +0530

    view payments logs

--- src/app/log/log.component.ts
+++ src/app/log/log.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { LogService } from '../service/log.service';
 import { LogItem } from '../interface/logItem.interface';
+import { PaymentService } from './payment.service.';
+import { Payment } from './payment.model.';
+import { VehicleAppointmentService } from '../vehicle-appointment/vehicle-appointment.service';
+import { Appointment } from '../vehicle-appointment/appointment.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-log',
@@ -9,21 +14,44 @@ import { LogItem } from '../interface/logItem.interface';
   styleUrls: ['./log.component.scss']
 })
 export class LogComponent implements OnInit {
-  vehicleLog : LogItem [];
+  payments : Payment [] = [];
+  selectedpayment : Payment;
+  appointments:Appointment[] = [];
+  selectedappointment:Appointment;
   isViewClicked: boolean = false;
   itemClicked:number;
+  private postsSub: Subscription;
 
-  constructor(private logService:LogService) {
-    this.vehicleLog = logService.getLogItems();
-    this.vehicleLog = this.vehicleLog.reverse();
+  constructor(private paymentservice:PaymentService,private appointmentService:VehicleAppointmentService) {
+
   }
 
   ngOnInit() {
+    this.paymentservice.getLogItems();
+    this.postsSub = this.paymentservice
+      .getPaymentUpdateListener()
+      .subscribe((payments: Payment[]) => {
+        this.payments = payments;
+      });
+  
+
+    this.appointmentService.getAppointments
+    this.postsSub = this.appointmentService
+      .getAppointmentUpdateListener()
+      .subscribe((appointments: Appointment[]) => {
+        this.appointments = appointments;
+      });
   }
 
-  onViewDetail(event:Event){
+  onViewDetail(i:number){
+   
     this.isViewClicked=true;
-    this.itemClicked = Number((<HTMLElement>event.target).id);
+    this.selectedpayment = this.payments[i];
+    this.appointments.forEach(app => {
+      if(app.id==this.selectedpayment.appid){
+        this.selectedappointment = app;
+      }
+    });
   }
 
   onDoneClicked(){
--- src/app/log/payment.service..ts
+++ src/app/log/payment.service..ts
@@ -4,16 +4,51 @@ import { Subject } from "rxjs";
 
 import { Router } from '@angular/router';
 import { Payment } from './payment.model.';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({ providedIn: "root" })
 export class PaymentService {
+ 
 
-  private payment: Payment[] = [];
+  private payments: Payment[] = [];
   private paymentUpdated = new Subject<Payment[]>();
 
 
   constructor(private http: HttpClient ,private router: Router) {}
+
+  getLogItems(){
+
+    this.http
+      .get<{ message: string; payments: any }>(
+        "http://localhost:3000/api/log"
+      )
+      .pipe(map((paymentData) => {
+        return paymentData.payments.map(payment => {
+          return {
+            packCharges: payment.packCharges,
+            oil: payment.vehicle,
+            airfiltering: payment.date,
+            additional: payment.time,
+            total: payment.package,
+            appid: payment.staffid,
+            id: payment._id
+          };
+      
+        });
+      }))
+      .subscribe(transformedPay => {
+        this.payments = transformedPay;
+        this.paymentUpdated.next([...this.payments]);
+        
+      });
+
+  }
+
+  getPaymentUpdateListener() {
+    return this.paymentUpdated.asObservable();
+  }
+
 
   addPayment(packCharges:string,oil: string, airfilttering: string,additional:string,total:string,appid:string) {
     const payment: Payment = { id: null,packCharges:packCharges, oil:oil, airfiltering:airfilttering,additional:additional,total:total,appid:appid};
@@ -24,8 +59,8 @@ export class PaymentService {
         const id = responseData.payid;
         console.log("id:"+id);
         payment.id = id;
-        this.payment.push(payment);
-        this.paymentUpdated.next([...this.payment]);
+        this.payments.push(payment);
+        this.paymentUpdated.next([...this.payments]);
       });
   }
   
